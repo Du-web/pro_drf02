21dfc8ab16789896163deadc847a3bd6e8517186
dudu <648302938@qq.com>
2020-09-13 11:37:05 +0800

    给user类视图添加get方法

--- userapp/urls.py
+++ userapp/urls.py
@@ -4,4 +4,5 @@ from userapp import views
 
 urlpatterns = [
     path('user/', views.UserAPIView.as_view()),
+    path('user/<str:id>/', views.UserAPIView.as_view()),
 ]
--- userapp/views.py
+++ userapp/views.py
@@ -4,10 +4,34 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from userapp.models import User
+
 
 class UserAPIView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response({
-            'status': 200,
-            'message': 'ok',
-        })
+        id = kwargs.get('id')
+        name = request.GET.get('username')
+        print(id, name)
+        print(request.query_params)
+        print(request.query_params.get('username'))
+        if id:
+            user = User.objects.filter(pk=id).values('name', 'pwd', 'gender').first()
+            if user:
+                return Response({
+                    'status': 200,
+                    'message': 'ok',
+                    'results': user
+                })
+            else:
+                return Response({
+                    'status': 500,
+                    'message': '用户不存在 ',
+
+                })
+        else:
+            users = User.objects.all().values('name', 'pwd', 'gender')
+            return Response({
+                'status': 200,
+                'message': 'ok',
+                'results': list(users)
+            })
