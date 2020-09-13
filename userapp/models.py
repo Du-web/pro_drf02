2b22d7ba52cf5cbe02280b22143d8354580f3f07
dudu <648302938@qq.com>
2020-09-13 10:34:17 +0800

    在models文件里创建user表，并迁移到数据库

--- userapp/models.py
+++ userapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    gender_choices = (
+        (0, 'male'),
+        (1, 'female')
+    )
+    name = models.CharField(max_length=50)
+    pwd = models.CharField(max_length=50)
+    gender = models.SmallIntegerField(choices=gender_choices, default=0)
+
+    class Meta:
+        db_table = 'db_user'
